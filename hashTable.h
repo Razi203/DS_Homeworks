daaf4283b60d61f0801b5e5906dbd1687d0f8be2
Razi Hleihil <30razi11@gmail.com>
2024-08-02 14:16:14 +0300

    hash setup 2

--- hashTable.h
+++ hashTable.h
@@ -2,7 +2,35 @@
 #define HASHTABLE_H
 
 #include <memory>
+#include "consts.h"
 using std::shared_ptr;
+
+template <typename T>
+class HashNode
+{
+public:
+    HashNode(int key) : id(key) {}
+
+    shared_ptr<T> getValue()
+    {
+        return value;
+    }
+
+    int getId()
+    {
+        return id;
+    }
+
+    shared_ptr<HashNode<T>> getNext()
+    {
+        return next;
+    }
+
+private:
+    int id;
+    shared_ptr<T> value;
+    shared_ptr<HashNode<T>> next;
+};
 
 template <typename T>
 class HashTable
@@ -12,7 +40,7 @@ public:
 
     ~HashTable();
 
-    shared_ptr<T> &insert(int key);
+    shared_ptr<HashNode<T>> &insert(int key);
 
     void remove(int key);
 
@@ -27,9 +55,18 @@ public:
 private:
     int hash(int key);
 
-    int table_size = 100;
-    int size = 0;
-    shared_ptr<T> table[table_size];
+    int table_size = INITIAL_TABLE_SIZE;
+    int size = ZERO;
+    shared_ptr<HashNode<T>> table[table_size];
 };
+
+template <typename T>
+HashTable<T>::HashTable()
+{
+    for (int i = 0; i < table_size; i++)
+    {
+        table[i] = nullptr;
+    }
+}
 
 #endif // HASHTABLE_H
